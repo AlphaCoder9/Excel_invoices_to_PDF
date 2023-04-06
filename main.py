31a2c67914003b15d159787c90e0cfe15ee66c90
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-04-06 01:44:53 -0400

    Add load the Excel into Dataframe.

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import glob  # standard library
+import openpyxl
+
+filepaths = glob.glob('invoices/*.xlsx')
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+    print(df)
