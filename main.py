1c482e789e9aad13ba8ee45a9582a38e18ac6cd7
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-04-07 03:51:08 -0400

    add minimal pdf too each excel file

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import glob  # standard library
 import openpyxl
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob('invoices/*.xlsx')
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
-    print(df)
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filename.split('-')[0]
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}')
+    pdf.output(f'PDFs/{filename}.pdf')
+
