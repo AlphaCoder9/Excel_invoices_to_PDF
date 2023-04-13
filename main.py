1a8f1726cd74c7b69325aedce8eb4db5736a58c7
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-04-13 16:25:46 -0400

    Add a table

--- main.py
+++ main.py
@@ -20,13 +20,23 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f'Date: {Date}', ln=1)
 
     df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    column = df.columns
+    column = [item.replace("_", " ").title() for item in column]
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=column[0], border=1)
+    pdf.cell(w=60, h=8, txt=column[1], border=1)
+    pdf.cell(w=35, h=8, txt=column[2], border=1)
+    pdf.cell(w=30, h=8, txt=column[3], border=1)
+    pdf.cell(w=30, h=8, txt=column[4], border=1, ln=1)
+
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
     pdf.output(f'PDFs/{filename}.pdf')
-
